cb19d36100126e1311ea2f2e12672dccce65b421
Slogiker <86723599+slogiker@users.noreply.github.com>
2025-09-11 20:01:12 +0200

    Add project files.

---
+++ Cansat Dashboard.Web/Data/Sensor.cs
@@ -0,0 +1,11 @@
+﻿// 📄 Data/Sensor.cs
+
+// IMPORTANT: Change "CanSatDashboard" to the actual name of your project.
+namespace CanSatDashboard.Data
+{
+    public class Sensor
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
+    }
+}
